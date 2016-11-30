146019e8443cd4f50d154c0a24b3b7f2140e6807
divyanshu.jain <divyanshujain12@hotmail.com>
2016-11-30 18:46:50 +0530

    sign up

--- app/src/main/java/com/example/divyanshujain/edoteng/LoginActivity.java
+++ app/src/main/java/com/example/divyanshujain/edoteng/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.divyanshujain.edoteng;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+    public void signUp(View view) {
+        startActivity(new Intent(this, SignUpActivity.class));
     }
 }
--- app/src/main/java/com/example/divyanshujain/edoteng/SignUpActivity.java
+++ app/src/main/java/com/example/divyanshujain/edoteng/SignUpActivity.java
@@ -1,13 +1,40 @@
 package com.example.divyanshujain.edoteng;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+
+import com.example.divyanshujain.edoteng.Utils.CommonFunctions;
+import com.neopixl.pixlui.components.button.Button;
+import com.neopixl.pixlui.components.edittext.EditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class SignUpActivity extends AppCompatActivity {
+    @InjectView(R.id.toolbarView)
+    Toolbar toolbarView;
+    @InjectView(R.id.usernameET)
+    EditText usernameET;
+    @InjectView(R.id.emailET)
+    EditText emailET;
+    @InjectView(R.id.phoneET)
+    EditText phoneET;
+    @InjectView(R.id.passwordET)
+    EditText passwordET;
+    @InjectView(R.id.confPasswordET)
+    EditText confPasswordET;
+    @InjectView(R.id.activity_sign_up)
+    LinearLayout activitySignUp;
+    @InjectView(R.id.signUpBT)
+    Button signUpBT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.inject(this);
+        CommonFunctions.getInstance().configureToolbarWithBackButton(this, toolbarView, getString(R.string.sign_up));
     }
 }
